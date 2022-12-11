871e2dae11c206c342429b553e9baaad13c3412e
motopolar <120253012+motopolar@users.noreply.github.com>
2022-12-11 04:00:00 -0800

    PolBot

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,11 +1,11 @@
 import { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } from '@bochilteam/scraper'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `🤖 Ingrese link de Instagram  🤏🏻😎`
+    if (!args[0]) throw `Ingrese link de Instagram  😊`
     const results = await instagramdl(args[0])
         .catch(async _ => await instagramdlv2(args[0]))
         .catch(async _ => await instagramdlv3(args[0]))
         .catch(async _ => await instagramdlv4(args[0]))
-    for (const { url } of results) await conn.sendFile(m.chat, url, 'instagram.mp4', `🤖 Descarga Completa 🤏🏻😎`, m)
+    for (const { url } of results) await conn.sendFile(m.chat, url, 'instagram.mp4', `Tu pedido 😉`, m)
 }
 handler.help = ['instagram <link ig>']
 handler.tags = ['downloader']
