26c1d190109d697ac2e005a6ed95bb86caa3effe
motopolar <120253012+motopolar@users.noreply.github.com>
2022-12-11 18:49:01 -0800

    𝓟𝓸𝓵𝓑𝓸𝓽

--- plugins/downloader-igstory.js
+++ plugins/downloader-igstory.js
@@ -10,15 +10,15 @@ let handler = async (m, { conn, args, usedPrefix, command, text}) => {
 ╽🤖️ Escriba el Nombre de
 ┃Una cuenta de Instagram
 ┃\n\n
-┃🤖 Ejemplo : ${usedPrefix + command} auronplay
-╰•⋆҈͜͡ ⫘𝐓𝐔𝐑𝐁𝐎⋆𝐍𝐈𝐓𝐑𝐎⫘⋆҈͜͡•╯`
+┃🤖 Ejemplo : ${usedPrefix + command} rosalia.vt
+╰•⋆҈͜͡𝓟𝓸𝓵𝓑𝓸𝓽•╯`
        
   await m.reply(wait)
   
    hx.igstory(text).then(async (result) => {
           for (let i of result.medias) {
             if (i.url.includes("mp4")) {            
-              conn.sendFile(m.chat, i.url, 'igstory.mp4', `🤖 Tú pedido 🤖`.trim(), m)
+              conn.sendFile(m.chat, i.url, 'igstory.mp4', `🤖 Tu amigo Polar ha descargado esto especialmente para ti y con mucho cariño.`.trim(), m)
             } else {     
               conn.sendFile(m.chat, i.url, '', '', m)
             }
