3600a4c49d19f691e6baf80371f42bb58d559db1
motopolar <120253012+motopolar@users.noreply.github.com>
2022-12-11 18:53:23 -0800

    𝓟𝓸𝓵𝓑𝓸𝓽

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,11 +1,12 @@
 import { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } from '@bochilteam/scraper'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Ingrese link de Instagram  😊`
+    if (!args[0]) throw `Hola, soy tu amigo Polar. Estoy aquí para explicarte como funciona este comando. Para hacer utilidad de este, debes escribir el comando y un link de Instagram. Puedes usar enlaces de reels o publicaciones normales. 😉
+    Ejemplo: /ig https://www.instagram.com/p/Ci8Dr-puBHJ/?igshid=ZmRlMzRkMDU= 🤗`
     const results = await instagramdl(args[0])
         .catch(async _ => await instagramdlv2(args[0]))
         .catch(async _ => await instagramdlv3(args[0]))
         .catch(async _ => await instagramdlv4(args[0]))
-    for (const { url } of results) await conn.sendFile(m.chat, url, 'instagram.mp4', `Tu pedido 😉`, m)
+    for (const { url } of results) await conn.sendFile(m.chat, url, 'instagram.mp4', `Tu amigo Polar ha descargado esto especialmente para ti. 😉`, m)
 }
 handler.help = ['instagram <link ig>']
 handler.tags = ['downloader']
